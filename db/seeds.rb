55dfc1a3a7e2cf75da7815df54fb9676056989a0
Oskar Perskaas <oskar.perskaas@gmail.com>
2016-10-05 11:27:49 -0400

    Add User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.destroy_all
+porkchop = User.create!(username: 'porkchop', password: 'porkchop')
+opp = User.create!(username: 'opp', password: 'porkchop')
+porkchop2 = User.create!(username: 'porkchop2', password: 'porkchop')
