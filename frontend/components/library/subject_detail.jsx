ddac9e1f9e3fde998cc94de318db3a0bda1ed916
Oskar Perskaas <oskar.perskaas@gmail.com>
2016-10-07 14:04:48 -0400

    Style subject-detail header

--- frontend/components/library/subject_detail.jsx
+++ frontend/components/library/subject_detail.jsx
@@ -21,11 +21,19 @@ class SubjectDetail extends React.Component {
     return (
       <div className='subject-detail'>
         <div className='subject-detail-main-col'>
-          <header className='subject-detail-header'>
-            <h2>{this.subject.title}</h2>
-            <div className='progress-holder'>
-              <div style={progressStyle}className='progress'></div>
+          <header className='subject-detail-header group'>
+            <div className='large-icon-holder'>
+              <i className="fa fa-book deck-icon" ></i>
             </div>
+            <div className='subject-detail-title'>
+              <h2>{this.subject.title}</h2>
+              <div className='progress-holder'>
+                <div style={progressStyle}className='progress'></div>
+              </div>
+            </div>
+            <button className='menu-button'>
+              <i className="fa fa-cog" ></i>
+            </button>
           </header>
         </div>
       </div>
