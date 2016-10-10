dadb29d151550a692605eebc047c41a1a9a0e04d
Oskar Perskaas <oskar.perskaas@gmail.com>
2016-10-10 16:32:14 -0400

    Style study-btns

--- frontend/components/study/card_study.jsx
+++ frontend/components/study/card_study.jsx
@@ -17,31 +17,33 @@ class CardStudy extends React.Component {
     this.setState({revealed: false});
   }
 
+  componentDidMount() {
+    this.props.fetchCards
+  }
 
   render() {
     let buttons;
+    let howWellText = ' ';
+    let sideText = 'Q.'
     if (this.state.revealed) {
       buttons = (
-        <ul>
-          <li>
-            <button className='rating-btn one' onClick={this.rateOne}>1 <small>Not At All</small></button>
-          </li>
-          <li>
-            <button className='rating-btn two'>2</button>
-          </li>
-          <li>
-            <button className='rating-btn three'>3</button>
-          </li>
-          <li>
-            <button className='rating-btn four'>4</button>
-          </li>
-          <li>
-            <button className='rating-btn five'>5<small>Perfectly</small></button>
-          </li>
-        </ul>
-      )
+        <div className='card-btns'>
+          <button className='rating-btn one' onClick={this.rateOne}>1<small>Not At All</small></button>
+          <button className='rating-btn two'>2</button>
+          <button className='rating-btn three'>3</button>
+          <button className='rating-btn four'>4</button>
+          <button className='rating-btn five'>5<small>Perfectly</small></button>
+        </div>
+      );
+      howWellText = 'How well did you know this?';
+      sideText = 'A.'
     } else {
-      buttons = <button onClick={this.revealAnswer}>Reveal Answer</button>
+      let className = `reveal-btn`
+      buttons = (
+        <div className='card-btns'>
+          <button className={className} title='Press space on your keyboard' onClick={this.revealAnswer}>Reveal Answer</button>
+        </div>
+      );
     }
     return (
       <main className='card-study'>
@@ -49,7 +51,10 @@ class CardStudy extends React.Component {
         <div className='card-front'>
 
         </div>
-        {buttons}
+        <div className='study-toolbar'>
+          <p>{howWellText}</p>
+          {buttons}
+        </div>
       </main>
     );
   }
