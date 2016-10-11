420b5d6c0ff49c8c7f45353f5ac6e74b7a2d3abf
Oskar Perskaas <oskar.perskaas@gmail.com>
2016-10-11 17:22:16 -0400

    Style search page

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 class Search extends React.Component {
   render () {
     return (
-      <p>Dis Da SEarch Component</p>
+      <div className='search-cont'>
+        <div className='search'>
+          <h1>What would you like to learn today?</h1>
+          <div className='search-bar'>
+            <strong>
+              <i className="fa fa-search"></i>
+            </strong>
+            <form className='search-form group'>
+              <input type='text'
+                placeholder='(e.g. MCAT, Norwegian, React)'>
+
+              </input>
+              <button className='search-btn'>Search</button>
+            </form>
+          </div>
+          <h3>Search over a dozen subjects...</h3>
+        </div>
+      </div>
     );
   }
 }
