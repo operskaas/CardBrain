69371be20e67455573044adc8cfe6b30b456fcc3
Oskar Perskaas <oskar.perskaas@gmail.com>
2016-10-17 18:52:21 -0400

    Fix React v15 minification warning

---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: './frontend/card_brain.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
